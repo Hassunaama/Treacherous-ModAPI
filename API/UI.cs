ffdcb86b3e8dcfb15bff998d769a4f9f92881a14
Nino Kröger <nino.kroger@gmail.com>
2022-12-17 18:03:29 +0200

    Update UI.cs

--- API/UI.cs
+++ API/UI.cs
@@ -4,13 +4,13 @@ using System.Text;
 using BepInEx;
 using BepInEx.Configuration;
 
+using JetBrains.Annotations;
+
 using Treacherous_Modder;
 using Treacherous_Modder.Misc;
 
-namespace Treacherous_Modder.API
-{
-    public static class UI
-    {
-
-    }
+namespace Treacherous_Modder.API;
+[PublicAPI]
+public static class UI {
+        
 }
